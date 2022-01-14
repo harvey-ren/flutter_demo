164c024c61d19600f4f648ea90269796804781ea
harvey <hxharveydev@gmail.com>
2022-01-14 16:19:38 +0700

    mod

--- lib/widget/navigation_bar.dart
+++ lib/widget/navigation_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-
+// https://www.jianshu.com/p/84fb9861a1f4
 class NavigationBar extends StatelessWidget {
   const NavigationBar({Key key}) : super(key: key);
 
